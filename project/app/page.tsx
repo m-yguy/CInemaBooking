5b3ab7f3b154ab776190c6b0487a67878aba57d1
SahirChowdhury2174 <sahirc2184@gmail.com>
2026-02-23 23:43:20 -0500

    Backend connected to front end (mostly?)

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -1,24 +1,18 @@
+"use client"
 import Navbar from "./components/Navbar";
 import MovieCard from "./components/MovieCard";
+import { useEffect, useState } from "react";
+import { movie } from "./types/movie";
 
 export default function Home() {
-  const tempMovieData = {
-    title: "28 Years Later",
-    genre: "Horror/Thriller",
-    rating: 8.8,
-    description:
-      "It's been almost three decades since the rage virus escaped from a biological weapons laboratory. Still living in a ruthlessly enforced quarantine, some have found ways to exist amid the infected. One such group of survivors lives on a small island connected to the mainland by a single, heavily defended causeway. When one of them decides to venture into the dark heart of the mainland, he soon discovers a mutation that has spread to not only the infected, but other survivors as well.",
-    poster_path: "/Movie_Posters/28YearsLater.jpg",
-    showtime: "2026-05-01 19:30:00",
-    trailer_link: "https://www.youtube.com/watch?v=IYGG55qwQZQ",
-    release_status: "Now Playing",
-    mpa_rating: "R",
-    cast: "Jodie Corner, Alfie Williams, Aaron Taylor-Johnson, Ralph Fiennes",
-    director: "Danny Boyle",
-    producer: "Danny Boyle, Alex Garland, Andrew Macdonald",
-    runtime: 115,
-  };
-  //This is currently using this dummy data, ideally I assume we'll just loop through the fetched data and display them all
+
+  const [movies, setMovies] = useState<movie[]>([]);
+  useEffect(() => {
+    fetch("/api/test")
+      .then(res => res.json())
+      .then(data => setMovies(data))
+  }, [])
+
 
   return (
     <div className="flex flex-col overflow-hidden">
@@ -26,14 +20,9 @@ export default function Home() {
       <main className="">
         <div className="bg-black">hero</div>
         <div className="grid gap-6 grid-cols-[repeat(auto-fit,minmax(250px,1fr))]">
-          <MovieCard movieData={tempMovieData} />
-          <MovieCard movieData={tempMovieData} />
-          <MovieCard movieData={tempMovieData} />
-          <MovieCard movieData={tempMovieData} />
-          <MovieCard movieData={tempMovieData} />
-          <MovieCard movieData={tempMovieData} />
-          <MovieCard movieData={tempMovieData} />
-          <MovieCard movieData={tempMovieData} />
+          {movies.map((movie) => (
+            <MovieCard key={movie.title} movieData={movie}/>
+          ))}
         </div>
       </main>
     </div>
