5805cdfab7e7074e7b975834efa9cfa778154a37
Jeffrey Luo <sinkinsheep@gmail.com>
2026-03-30 13:44:43 -0400

    fix formatting

--- project/auth/actions.ts
+++ project/auth/actions.ts
@@ -82,7 +82,8 @@ export async function login(formData: FormData) {
   const email = formData.get("email") as string;
 
   const sqlClient = neon(process.env.DATABASE_URL!);
-  const users = await sqlClient`SELECT user_type FROM users WHERE email = ${email} LIMIT 1`;
+  const users =
+    await sqlClient`SELECT user_type FROM users WHERE email = ${email} LIMIT 1`;
   const redirectTo = users[0]?.user_type === "ADMIN" ? "/admin" : "/";
 
   try {
