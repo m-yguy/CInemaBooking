deffb43871e88ea8415b1d5a85253b68748e78f1
Jeffrey Luo <sinkinsheep@gmail.com>
2026-04-14 15:32:36 -0400

    change db.ts into singletone pattern to have global access and avoiding having multiple instances.

--- project/lib/db.ts
+++ project/lib/db.ts
@@ -1,3 +1,13 @@
-import { neon } from "@neondatabase/serverless";
+import { neon, NeonQueryFunction } from "@neondatabase/serverless";
 
-export const sql = neon(process.env.DATABASE_URL!);
+declare global {
+  var __db: NeonQueryFunction<false, false> | undefined;
+}
+
+const db = globalThis.__db ?? neon(process.env.DATABASE_URL!);
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.__db = db;
+}
+
+export const sql = db;
